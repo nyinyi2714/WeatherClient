c7f4670f6f9eea719b9295d3e702b02b87d4149c
Htet <nyinyimoe.htet.281@my.csun.edu>
2024-02-23 20:38:37 -0800

    navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HelloComponent } from './hello/hello.component';
+import { CountryComponent } from './country/country.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: HelloComponent, pathMatch: "full" },
+    { path: "countries", component: CountryComponent },
+];
